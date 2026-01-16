1b4a32f090545d5481992337879d0cc988eec0fd
katri <katrinaalmeida77@gmail.com>
2026-01-16 16:00:35 -0500

    multiplicacion. v.1.3

---
+++ Multiplicacion.h
@@ -0,0 +1,13 @@
+#ifndef MULTIPLICACION_H
+#define MULTIPLICACION_H
+
+class Multiplicacion {
+private:
+    float a, b;
+
+public:
+    void ingresarDatos();
+    void calcularMultiplicacion();
+};
+
+#endif
