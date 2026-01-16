dabba82fd74e17c556958fac5a91c0fc03b9ce3e
katri <katrinaalmeida77@gmail.com>
2026-01-16 15:56:48 -0500

    Cambios con archivo .h y .cpp de multiplicacion

--- multiplicacion.cpp
+++ multiplicacion.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
+#include "Multiplicacion.h"
+
 using namespace std;
 
-// Definición de la clase
-class Multiplicacion {
-private:
-    float a, b;
-
-public:
-    // Método para ingresar datos
-    void ingresarDatos() {
-        cout << "Ingrese el primer numero: ";
-        cin >> a;
-        cout << "Ingrese el segundo numero: ";
-        cin >> b;
-    }
-
-    // Método para realizar la multiplicación
-    void calcularMultiplicacion() {
-        cout << "La multiplicacion es: " << a * b << endl;
-    }
-};
-
-// Función principal
-int main() {
-    Multiplicacion obj;
-    obj.ingresarDatos();
-    obj.calcularMultiplicacion();
-    return 0;
+void Multiplicacion::ingresarDatos() {
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
 }
+
+void Multiplicacion::calcularMultiplicacion() {
+    cout << "La multiplicacion es: " << a * b << endl;
+}
+
